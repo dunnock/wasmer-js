4fa7973c4c56425e735d52818c4881b225138ac8
Syrus <me@syrusakbary.com>
2019-10-18 11:25:25 -0700

    Updated xterm to 4.x

--- packages/wasm-terminal/lib/wasm-tty/wasm-tty.ts
+++ packages/wasm-terminal/lib/wasm-tty/wasm-tty.ts
@@ -110,9 +110,20 @@ export default class WasmTTY {
   /**
    * Prints a message and properly handles new-lines
    */
-  print(message: string) {
-    const normInput = message.replace(/[\r\n]+/g, "\n");
-    this.xterm.write(normInput.replace(/\n/g, "\r\n"));
+  print(message: string, sync?: boolean) {
+    const normInput = message.replace(/[\r\n]+/g, "\n").replace(/\n/g, "\r\n");
+    if (sync) {
+      // Force a reload in xterm
+      //@ts-ignore
+      this.xterm._core.writeSync(normInput);
+      //@ts-ignore
+      this.xterm._core._renderService._renderer._runOperation(renderer =>
+        renderer.onGridChanged(0, this.xterm.rows - 1)
+      );
+    } else {
+      //@ts-ignore
+      this.xterm._core.write(normInput);
+    }
   }
 
   /**
